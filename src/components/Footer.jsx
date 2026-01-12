a34f557b28c33f6fe403fcb00044c2fb2a34ec43
Eva Tokmakova <eva.tokmakova12@gmail.com>
2026-01-12 22:24:51 +0500

    completed style files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="links">
+        <h2>Ожидаю вас у себя</h2>
+        <p>Иванов Иван Иванович</p>
+        <div className="footer-btns">
+          <button>Инстаграмм</button>
+          <button>Написать письмо</button>
+        </div>
+      </div>
+      <div className="footer-info">
+        <p>2026 ©️ Иванов Иван</p>
+        <p>Создано: ❤️</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
