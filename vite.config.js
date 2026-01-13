5d5401fbd3d8bc2946d456cb93d6cbe2423715aa
Eva Tokmakova <eva.tokmakova12@gmail.com>
2026-01-13 15:16:57 +0500

    addednetlify file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/landing-project1/",
+
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
+
+  server: {
+    port: 3000,
+  },
 });
